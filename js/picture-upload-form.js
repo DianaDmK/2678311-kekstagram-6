9e2caf4c4d6c312f98694153a4a31c8217014353
DianaDmK <kalyakina.130802@mail.ru>
2025-12-15 22:30:21 +0700

    feat: 9.11. Truth or Dare

--- js/picture-upload-form.js
+++ js/picture-upload-form.js
@@ -1,3 +1,56 @@
-// const uploadForm = document.querySelector('.img-upload__form');
+import { uploadForm, pristine } from './validate.js';
 
-// new Pristine(uploadForm);
+const uploadInput = document.querySelector('.img-upload__input');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const pictureFormClose = document.querySelector('.img-upload__cancel');
+const body = document.querySelector('body');
+
+const openUploadForm = () => {
+  uploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  uploadForm.addEventListener('submit', onSubmit);
+
+  document.addEventListener('keydown', closeByEscape);
+  pictureFormClose.addEventListener('click', closeByButton);
+};
+
+const closeUploadForm = () => {
+  uploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+  uploadInput.value = '';
+
+  if (uploadForm) {
+    uploadForm.reset();
+  }
+
+  uploadForm.removeEventListener('submit', onSubmit);
+  document.removeEventListener('keydown', closeByEscape);
+};
+
+uploadInput.addEventListener('change', () => {
+  openUploadForm();
+});
+
+function closeByEscape(evt) {
+  if (evt.key === 'Escape' && evt.target.tagName !== 'INPUT') {
+    evt.preventDefault();
+    closeUploadForm();
+  }
+
+}
+
+function closeByButton() {
+  closeUploadForm();
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  console.log(isValid);
+  if (!isValid) {
+    return false;
+  }
+}
+
