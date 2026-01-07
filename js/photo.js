b56ca9c18d0cd9d97d1a1320cdc29e3fffd0d2c7
DianaDmK <kalyakina.130802@mail.ru>
2026-01-08 00:48:09 +0700

    feat: 12.11. Keepsake photo

--- js/photo.js
+++ js/photo.js
@@ -1,7 +1,8 @@
-const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+import { FILE_TYPES } from './consts.js';
 
 const uploadInput = document.querySelector('.img-upload__start input[type=file]');
 const preview = document.querySelector('.img-upload__preview img');
+const effectPreviews = document.querySelectorAll('.effects__preview');
 
 const setFormPhoto = () => {
   const file = uploadInput.files[0];
@@ -10,7 +11,11 @@ const setFormPhoto = () => {
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
-    preview.src = URL.createObjectURL(file);
+    const url = URL.createObjectURL(file);
+    preview.src = url;
+    effectPreviews.forEach((el) => {
+      el.style.backgroundImage = `url(${url})`;
+    });
   }
 };
 
